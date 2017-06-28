7e25474fd0c8b891118c0e37ff49fbae48a6fba0
jtmelton <jtmelton@gmail.com>
2017-06-28 01:17:04 -0400

    Initial Import.

---
+++ src/main/java/org/mannasecurity/VcsApplication.java
@@ -0,0 +1,12 @@
+package org.mannasecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VcsApplication.class, args);
+    }
+}
